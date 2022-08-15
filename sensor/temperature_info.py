0d6f464f6c51bfffc7a23ccf3f8175dfeeca77b7
bibhutisingh2017 <110843648+bibhutisingh2017@users.noreply.github.com>
2022-08-15 19:55:47 +0530

    Completed module

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -15,4 +15,4 @@ class TemperatureData(HouseInfo):
 
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date("temperature", rec_date)
-        return self._convert_data(recs) 
+        return self._convert_data(recs)
